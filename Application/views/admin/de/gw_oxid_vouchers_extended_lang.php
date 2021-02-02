4593263eee1349d3c607bdb0c2d3dd06f4a84881
Gregor Wendland <gregor@gewend.de>
2021-02-02 14:41:07 +0100

    improve option description

--- Application/views/admin/de/gw_oxid_vouchers_extended_lang.php
+++ Application/views/admin/de/gw_oxid_vouchers_extended_lang.php
@@ -10,8 +10,8 @@ $aLang = array(
 	'GW_ONLY_ONCE_PER_SHIPPING_ADDRESS' => 'Gutscheine dieser Serie nur einmal pro Liefer-Adresse gültig',
 	'HELP_GW_ONLY_ONCE_PER_SHIPPING_ADDRESS' => 'Dies soll etwas stärker verhindern, dass Kunden mehrmals durch mehrmaliges Registrieren mit verschiedenen E-Mail-Adressen, den Gutscheincode verwenden können.',
 
-	'GW_ONLY_NOT_REDUCED_ARTICLES' => 'Nicht gültig für reduzierte und rabattierte Artikel',
-	'HELP_GW_ONLY_NOT_REDUCED_ARTICLES' => 'Wenn gesetzt, sind Gutscheine dieser Serie nicht gültig.',
+	'GW_ONLY_NOT_REDUCED_ARTICLES' => 'Nicht gültig für bereits reduzierte Artikel',
+	'HELP_GW_ONLY_NOT_REDUCED_ARTICLES' => 'Wenn gesetzt, sind Gutscheine dieser Serie nicht gültig für in irgendeiner Weise bereits redzuierte (UVP < Artikelpreis) und/oder rabattierte (Preis reduziert durch gnerellen Rabatt) Artikel.',
 
 	'GW_HANDLE_LIKE_DISCOUNT' => 'Gutscheine bei Bestellabschluss in Rabatt umwandeln',
 	'HELP_GW_HANDLE_LIKE_DISCOUNT' => 'Wandelt den absoluten Wert der Gutscheine dieser Serie beim Abschluss der Bestellung in einen Rabatt um.',
