8a6b1ce7d67aa71b50fe802140db6dd3ade3d5ad
Gregor Wendland <gregor@gewend.de>
2021-02-12 12:02:42 +0100

    bugfix skip discount was not handled correctly when transforming voucher to discount

--- Application/Model/Discount.php
+++ Application/Model/Discount.php
@@ -23,8 +23,10 @@ class Discount extends Discount_parent {
 			if(
 				(double)$oArticle->getPrice()->getPrice() < (double)$oArticle->getBasePrice() // calculated price is smaller than standard price -> it's reduced
 				|| ( method_exists($oArticle, 'getTPrice') && $oArticle->getTPrice() && (double)$oArticle->getPrice()->getPrice() < (double)$oArticle->getTPrice()->getPrice() ) // RRP (UVP) is bigger than price it's
+
 				|| ($oArticle->isOrderArticle() && $oArticle->getArticle()->getTPrice() && (double)$oArticle->getArticle()->getPrice()->getPrice() < (double)$oArticle->getArticle()->getTPrice()->getPrice() ) // RRP (UVP) of OrderArticles Article object is bigger than price it's
 				|| ($oArticle->isOrderArticle() && (double)$oArticle->getArticle()->getPrice()->getPrice() < (double)$oArticle->getArticle()->getBasePrice() )  // calculated price of OrderArticles Article object is smaller than standard price -> it's reduced
+				|| ($oArticle->isOrderArticle() && $oArticle->getArticle()->skipDiscounts())  // 
 			) {
 				return false;
 			} elseif($this->oxdiscount__gw_is_product_voucher->value || $this->oxdiscount__gw_is_category_voucher->value) {
